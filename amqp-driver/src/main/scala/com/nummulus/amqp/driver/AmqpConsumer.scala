fcf70a0b39b45ae8713c5cdac6cecfdb8fd24874
Johan Flint <johan.flint@nummulus.com>
2013-08-08 22:29:01 +0200

    NUM-115 Defined an interface for the Driver

---
+++ amqp-driver/src/main/scala/com/nummulus/amqp/driver/AmqpConsumer.scala
@@ -0,0 +1,6 @@
+package com.nummulus.amqp.driver
+
+/**
+ * A consumer of a service, which in turn is offered by a provider.
+ */
+trait AmqpConsumer {}
---
+++ amqp-driver/src/main/scala/com/nummulus/amqp/driver/AmqpDriver.scala
@@ -0,0 +1,15 @@
+package com.nummulus.amqp.driver
+
+/**
+ * A driver is a factory for service providers and consumers.
+ */
+trait AmqpDriver {
+  /**
+   * Returns a new consumer for a services' operation.
+   * 
+   * @param service name of the service owning the operation to consume
+   * @param operation name of the operation to consume
+   * @return new consumer
+   */
+  def newConsumer(service: String, operation: String): AmqpConsumer
+}
