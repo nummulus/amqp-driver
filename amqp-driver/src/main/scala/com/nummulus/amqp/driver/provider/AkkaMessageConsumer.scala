6f66dd300457c6f2c0bda9f7365efb2ee28169a2
Johan Flint <johan.flint@nummulus.com>
2013-09-01 22:10:19 +0200

    NUM-115 AkkaMessageConsumer written

---
+++ amqp-driver/src/main/scala/com/nummulus/amqp/driver/provider/AkkaMessageConsumer.scala
@@ -0,0 +1,13 @@
+package com.nummulus.amqp.driver.provider
+
+import com.nummulus.amqp.driver.Channel
+import com.nummulus.amqp.driver.MessageConsumer
+import com.rabbitmq.client.{Consumer => RabbitConsumer}
+
+import akka.actor.ActorRef
+
+class AkkaMessageConsumer(channel: Channel, actor: ActorRef) extends MessageConsumer {
+  private lazy val consumer = new AkkaRabbitConsumer(channel.get, actor)
+  
+  override private[driver] def get: RabbitConsumer = consumer
+}
---
+++ amqp-driver/src/main/scala/com/nummulus/amqp/driver/provider/AkkaRabbitConsumer.scala
@@ -0,0 +1,21 @@
+package com.nummulus.amqp.driver.provider
+
+import com.nummulus.amqp.driver.akka.AmqpMessage
+import com.rabbitmq.client.AMQP
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.DefaultConsumer
+import com.rabbitmq.client.Envelope
+
+import akka.actor.ActorRef
+
+/**
+ * Akka message consumer.
+ */
+class AkkaRabbitConsumer(channel: Channel, actor: ActorRef) extends DefaultConsumer(channel) {
+   override def handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: Array[Byte]) {
+     val message = new String(body)
+     val deliveryTag = envelope.getDeliveryTag
+     
+     actor ! AmqpMessage(message, deliveryTag)
+   }
+}
