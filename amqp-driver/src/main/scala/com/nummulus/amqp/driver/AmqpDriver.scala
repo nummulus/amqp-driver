6720e44070a922d6605550fcc99a96cd2c25b956
Johan Flint <johan.flint@nummulus.com>
2013-09-01 22:05:41 +0200

    NUM-115 Added a factory to create AmqpDriver instances

--- amqp-driver/src/main/scala/com/nummulus/amqp/driver/AmqpDriver.scala
+++ amqp-driver/src/main/scala/com/nummulus/amqp/driver/AmqpDriver.scala
@@ -1,5 +1,9 @@
 package com.nummulus.amqp.driver
 
+import com.rabbitmq.client.{ConnectionFactory => RabbitConnectionFactory}
+import com.typesafe.config.ConfigFactory
+
+
 /**
  * A driver is a factory for service providers and consumers.
  */
@@ -20,4 +24,21 @@ trait AmqpDriver {
    * @return new provider
    */
   def newProvider(operation: String): AmqpProvider
+}
+
+/**
+ * Factory for the AMQP driver.
+ */
+object AmqpDriver {
+  private val connectionFactory = new ConnectionFactory(new RabbitConnectionFactory)
+  
+  /**
+   * Returns a new driver with the configuration loaded from "application.conf".
+   */
+  def apply: AmqpDriver = new DefaultDriver(connectionFactory, ConfigFactory.load)
+  
+  /**
+   * Returns a new driver with the configuration loaded from the specified configuration file.
+   */
+  def apply(configFileName: String): AmqpDriver = new DefaultDriver(connectionFactory, ConfigFactory.load(configFileName)) 
 }
