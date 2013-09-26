e241638e81651fc148b1bb4a02a98b1baadab288
Johan Flint <johan.flint@nummulus.com>
2013-09-26 21:39:40 +0200

    NUM-115 AmqpProvider now uses the AkkaMessageConsumer instead of the BlockingMessageConsumer

--- amqp-driver/src/main/scala/com/nummulus/amqp/driver/AmqpProvider.scala
+++ amqp-driver/src/main/scala/com/nummulus/amqp/driver/AmqpProvider.scala
@@ -7,8 +7,6 @@ import _root_.akka.actor.ActorRef
  * A provider listens to a well-known queue and provides a certain service.
  */
 trait AmqpProvider {
-  def bindCallBack(serviceCallBack: (String) => String)
   def bind(actor: ActorRef)
   def unbind()
-  def handleNextDelivery()
 }
--- amqp-driver/src/main/scala/com/nummulus/amqp/driver/DefaultProvider.scala
+++ amqp-driver/src/main/scala/com/nummulus/amqp/driver/DefaultProvider.scala
@@ -1,91 +1,39 @@
 package com.nummulus.amqp.driver
 
 import org.slf4j.LoggerFactory
+
+import com.nummulus.amqp.driver.akka.AmqpGuardianActor
 import com.nummulus.amqp.driver.configuration.QueueConfiguration
-import com.nummulus.amqp.driver.consumer.Delivery
+import com.nummulus.amqp.driver.provider.AkkaMessageConsumer
+
 import _root_.akka.actor.ActorRef
-import _root_.akka.pattern.ask
-import _root_.akka.pattern.AskTimeoutException
-import _root_.akka.util.Timeout
-import scala.concurrent._
-import scala.concurrent.duration._
-import scala.util.Failure
-import scala.util.Success
-import scala.util.Failure
-import ExecutionContext.Implicits.global
+import _root_.akka.actor.ActorSystem
+import _root_.akka.actor.Props
 
 /**
  * Default provider implementation.
  */
 private[driver] class DefaultProvider(channel: Channel, configuration: QueueConfiguration) extends AmqpProvider {
-  implicit val timeout = Timeout(1.second)
   private val logger = LoggerFactory.getLogger(getClass)
+  private lazy val actorSystem = ActorSystem("AmqpDriver")
   
   private val requestQueue = channel.queueDeclare(configuration.queue, configuration.durable, configuration.exclusive, configuration.autoDelete, null)
   logger.debug("Declared request queue: {}", requestQueue.getQueue)
   
   channel.basicQos(1)
   
-  val callback = MessageConsumer.newBlocking(channel)
-  channel.basicConsume(requestQueue.getQueue, configuration.autoAcknowledge, callback)
-  
   var actor: Option[ActorRef] = None
-  var serviceCallBack: Option[(String) => String] = None
   
   def bind(actor: ActorRef) {
+    val guardianActor = actorSystem.actorOf(Props(classOf[AmqpGuardianActor], actor, channel, configuration.autoAcknowledge), actor.path + "guardian")
+    val callback = new AkkaMessageConsumer(channel, guardianActor)
+    
+    channel.basicConsume(requestQueue.getQueue, configuration.autoAcknowledge, callback)
+    
     this.actor = Some(actor)
-  }
-  
-  def bindCallBack(serviceCallBack: (String) => String) {
-    this.serviceCallBack = Some(serviceCallBack)
   }
   
   def unbind() {
     this.actor = None
-  }
-  
-  def handleNextDelivery(): Unit = {
-    future {
-      val delivery = callback.nextDelivery
-      if (isQuestion(delivery))
-        handleAsk(delivery)
-      else
-        handleTell(delivery)
-    }
-  }
-  
-  private def isQuestion(delivery: Delivery) = delivery.properties.correlationId != null
-  
-  private def handleTell(delivery: Delivery): Unit = {
-    actor foreach { _ ! delivery.body.toString }
-  }
-  
-  private def handleAsk(delivery: Delivery): Unit =  {
-    serviceCallBack foreach { s =>
-      val response = s(delivery.body.toString())
-      val properties = MessageProperties(correlationId = delivery.properties.correlationId)
-      logger.debug("Sending message to queue: {}", response)
-      logger.debug("Properties = {}", properties)
-      channel.basicPublish("", delivery.properties.replyTo, properties, response.getBytes)
-      channel.basicAck(delivery.deliveryTag, false)
-    }
-    
-    actor foreach { a =>
-      val response = a ? delivery.body.toString
-      response onComplete {
-        case Success(msg) =>
-          val properties = MessageProperties(correlationId = delivery.properties.correlationId)
-          logger.debug("Sending message to queue: {}", msg.toString)
-          logger.debug("Properties = {}", properties)
-          channel.basicPublish("", delivery.properties.replyTo, properties, msg.toString.getBytes)
-          channel.basicAck(delivery.deliveryTag, false)
-        case Failure(e: AskTimeoutException) =>
-          // TODO: do something smart when the response times out
-          e.printStackTrace()
-        case Failure(e) =>
-          // TODO: do something smart when the response fails
-          e.printStackTrace()
-      }
-    }
   }
 }
