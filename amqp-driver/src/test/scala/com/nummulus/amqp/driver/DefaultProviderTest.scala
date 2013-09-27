86c3b9ed43ed47f93125a4ad2c637918f562cebd
Johan Flint <johan.flint@nummulus.com>
2013-09-27 16:08:06 +0200

    NUM-115 DefaultProvider#bind test coverage

--- amqp-driver/src/test/scala/com/nummulus/amqp/driver/DefaultProviderTest.scala
+++ amqp-driver/src/test/scala/com/nummulus/amqp/driver/DefaultProviderTest.scala
@@ -1,13 +1,20 @@
 package com.nummulus.amqp.driver
 
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
 import org.mockito.Mockito._
 import org.scalatest._
 import org.scalatest.junit._
+
 import com.nummulus.amqp.driver.fixture.ProviderFixture
+import com.nummulus.amqp.driver.matcher.TypeMatcher
+import com.nummulus.amqp.driver.provider.AkkaMessageConsumer
+
+import _root_.akka.actor.ActorSystem
+import _root_.akka.testkit.TestProbe
 
 @RunWith(classOf[JUnitRunner])
-class DefaultProviderTest extends FlatSpec with Matchers {
+class DefaultProviderTest extends FlatSpec with Matchers with TypeMatcher {
   behavior of "DefaultProvider"
   
   it should "declare a request queue at construction time" in new ProviderFixture {
@@ -16,5 +23,22 @@ class DefaultProviderTest extends FlatSpec with Matchers {
   
   it should "set the QOS to one" in new ProviderFixture {
     verify (channel).basicQos(1)
+  }
+  
+  it should "consume messages from the queue after calling bind" in new ProviderFixture {
+    implicit val system = ActorSystem("Test")
+    val probe = TestProbe()
+    
+    provider.bind(probe.ref)
+    
+    val queueCaptor = ArgumentCaptor.forClass(classOf[String])
+    val autoAcknowledgeCaptor = ArgumentCaptor.forClass(classOf[Boolean])
+    val callbackCaptor = ArgumentCaptor.forClass(classOf[MessageConsumer])
+    
+    verify (channel).basicConsume(queueCaptor.capture(), autoAcknowledgeCaptor.capture(), callbackCaptor.capture())
+    
+    queueCaptor.getValue should be ("requestQueue")
+    autoAcknowledgeCaptor.getValue should be (true)
+    callbackCaptor.getValue should be (ofType[AkkaMessageConsumer])
   }
 }
