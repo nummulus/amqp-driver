1730837c4f25576882fe18090f8a49764361516d
Robbert Jan Grootjans <robbertjan.grootjans@nummulus.com>
2013-10-03 22:52:55 +0200

    NUM-115: Moved config to test file

--- amqp-driver-test/src/test/scala/com/nummulus/amqp/driver/fixture/ProviderConsumerFixture.scala
+++ amqp-driver-test/src/test/scala/com/nummulus/amqp/driver/fixture/ProviderConsumerFixture.scala
@@ -1,9 +1,48 @@
 package com.nummulus.amqp.driver.fixture
 
 import com.nummulus.amqp.driver.AmqpDriver
+import com.nummulus.amqp.driver.DefaultDriver
+import com.nummulus.amqp.driver.ConnectionFactory
+import com.typesafe.config.ConfigFactory
+import com.rabbitmq.client.{ ConnectionFactory => RabbitConnectionFactory }
+import com.nummulus.amqp.driver.configuration.QueueConfiguration
+import com.nummulus.amqp.driver.DefaultProvider
+import com.typesafe.config.Config
+import com.typesafe.config.impl.SimpleConfigObject
+import com.typesafe.config._
 
-class ProviderConsumerFixture {
-  val driver = AmqpDriver("ampq.conf")
+class ProviderConsumerFixture() {
+  val configuration = """
+  amqp {
+    host = localhost
+    defines {
+      serviceName = service.test
+      Test {
+        queue = Test
+        durable = false
+        exclusive = false
+        autoDelete = false
+        autoAcknowledge = false
+      }
+    }
+    uses {
+      service.test {
+        serviceName = service.test
+        Test {
+          queue = Test
+          durable = false
+          exclusive = false
+          autoDelete = false
+          autoAcknowledge = true
+        }
+      }
+    }
+  }"""
+
+  val config = ConfigFactory.parseString(configuration);
+  val connectionFactory = new ConnectionFactory(new RabbitConnectionFactory)
+  val driver = new DefaultDriver(connectionFactory, config)
   val consumer = driver.newConsumer("service.test", "Test")
   val provider = driver.newProvider("Test")
+
 }
