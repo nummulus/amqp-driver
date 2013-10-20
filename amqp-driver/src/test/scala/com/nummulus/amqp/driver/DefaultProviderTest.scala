7275c27d1318277829ee18a8ee3a43fe85b9f82a
Jan Ouwens <jan.ouwens@nummulus.com>
2013-10-20 14:40:26 +0200

    NUM-115: Used TestKit and added an unbind unit test

--- amqp-driver/src/test/scala/com/nummulus/amqp/driver/DefaultProviderTest.scala
+++ amqp-driver/src/test/scala/com/nummulus/amqp/driver/DefaultProviderTest.scala
@@ -3,18 +3,18 @@ package com.nummulus.amqp.driver
 import org.junit.runner.RunWith
 import org.mockito.ArgumentCaptor
 import org.mockito.Mockito._
+import org.mockito.Matchers._
 import org.scalatest._
 import org.scalatest.junit._
-
 import com.nummulus.amqp.driver.fixture.ProviderFixture
 import com.nummulus.amqp.driver.matcher.TypeMatcher
 import com.nummulus.amqp.driver.provider.AkkaMessageConsumer
-
 import _root_.akka.actor.ActorSystem
 import _root_.akka.testkit.TestProbe
+import _root_.akka.testkit.TestKit
 
 @RunWith(classOf[JUnitRunner])
-class DefaultProviderTest extends FlatSpec with Matchers with TypeMatcher {
+class DefaultProviderTest extends TestKit(ActorSystem("test-system")) with FlatSpecLike with Matchers with TypeMatcher with BeforeAndAfterAll {
   behavior of "DefaultProvider"
   
   it should "declare a request queue at construction time" in new ProviderFixture {
@@ -26,10 +26,7 @@ class DefaultProviderTest extends FlatSpec with Matchers with TypeMatcher {
   }
   
   it should "consume messages from the queue after calling bind" in new ProviderFixture {
-    implicit val system = ActorSystem("Test")
-    val probe = TestProbe()
-    
-    provider.bind(probe.ref)
+    provider.bind(testActor)
     
     val queueCaptor = ArgumentCaptor.forClass(classOf[String])
     val autoAcknowledgeCaptor = ArgumentCaptor.forClass(classOf[Boolean])
@@ -40,5 +37,16 @@ class DefaultProviderTest extends FlatSpec with Matchers with TypeMatcher {
     queueCaptor.getValue should be ("requestQueue")
     autoAcknowledgeCaptor.getValue should be (true)
     callbackCaptor.getValue should be (ofType[AkkaMessageConsumer])
+  }
+  
+  it should "not receive messages after an unbind" in new ProviderFixture {
+    provider.bind(testActor)
+    provider.unbind()
+    
+    verify (channel).basicCancel(anyString)
+  }
+
+  override def afterAll: Unit = {
+    TestKit.shutdownActorSystem(system)
   }
 }
