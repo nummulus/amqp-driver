8755e53bdb1c70476cb5118bd791521b856841a4
Jan Ouwens <jan.ouwens@nummulus.com>
2014-05-13 21:06:12 +0200

    NUM-144: Improved scaladoc

--- amqp-driver/src/main/scala/com/nummulus/amqp/driver/AmqpProvider.scala
+++ amqp-driver/src/main/scala/com/nummulus/amqp/driver/AmqpProvider.scala
@@ -2,11 +2,18 @@ package com.nummulus.amqp.driver
 
 import _root_.akka.actor.ActorRef
 
-
 /**
  * A provider listens to a well-known queue and provides a certain service.
  */
 trait AmqpProvider {
-  def bind(actor: ActorRef)
-  def unbind()
+  /**
+   * Activates the provider. All messages that appear on the queue
+   * are wrapped in an AmqpRequestMessage and sent to [[actor]].
+   */
+  def bind(actor: ActorRef): Unit
+
+  /**
+   * Unbinds the actor from the provider, and de-activates it.
+   */
+  def unbind(): Unit
 }
