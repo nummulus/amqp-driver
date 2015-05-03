f12dec0ad2b993e23bfb4c5ef871365a3d19ff2c
Johan Flint <johan.flint@nummulus.com>
2015-05-03 17:16:40 +0200

    NUM-169 Send an AmqpQueueMessageWithProperties instead of spying on internals

--- amqp-driver-test/src/test/scala/com/nummulus/amqp/driver/fixture/BoundProviderFixture.scala
+++ amqp-driver-test/src/test/scala/com/nummulus/amqp/driver/fixture/BoundProviderFixture.scala
@@ -1,20 +1,18 @@
 package com.nummulus.amqp.driver.fixture
 
-import org.mockito.ArgumentCaptor
 import org.mockito.Mockito._
-import org.mockito.Matchers._
 import org.scalatest.mock.MockitoSugar
 
 import com.nummulus.amqp.driver.Channel
 import com.nummulus.amqp.driver.DefaultProvider
-import com.nummulus.amqp.driver.MessageConsumer
+import com.nummulus.amqp.driver.MessageProperties
 import com.nummulus.amqp.driver.QueueDeclareOk
+import com.nummulus.amqp.driver.akka.AmqpQueueMessageWithProperties
 import com.nummulus.amqp.driver.configuration.QueueConfiguration
-import com.rabbitmq.client.AMQP.BasicProperties
-import com.rabbitmq.client.Envelope
 
 import akka.actor.ActorSystem
 import akka.testkit.TestProbe
+import akka.actor.ActorRef
 
 class BoundProviderFixture(autoAcknowledge: Boolean) extends MockitoSugar {
   val channel = mock[Channel]
@@ -30,19 +28,13 @@ class BoundProviderFixture(autoAcknowledge: Boolean) extends MockitoSugar {
   val probe = TestProbe()
   val provider = new DefaultProvider(system, channel, queueConfiguration)
   
-  provider.bind(probe.ref)
-  
-  val queueCaptor = ArgumentCaptor.forClass(classOf[String])
-  val autoAcknowledgeCaptor = ArgumentCaptor.forClass(classOf[Boolean])
-  val callbackCaptor = ArgumentCaptor.forClass(classOf[MessageConsumer])
-  
-  verify (channel).basicConsume(queueCaptor.capture(), autoAcknowledgeCaptor.capture(), anyString(), callbackCaptor.capture())
-  
-  val consumer = callbackCaptor.getValue.get
+  var guardianActor: ActorRef = null
+  provider.bind(actor => {
+    guardianActor = actor
+    probe.ref
+  })
   
   def sendMessage(deliveryTag: Int, body: String) {
-    val envelope = new Envelope(deliveryTag, false, "", "routing")
-    val properties = new BasicProperties().builder.correlationId("1e").build
-    consumer.handleDelivery("", envelope, properties, body.getBytes)
+    guardianActor ! AmqpQueueMessageWithProperties(body, MessageProperties(correlationId = "1e"), deliveryTag)
   }
 }
